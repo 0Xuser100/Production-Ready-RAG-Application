961307c8e8e1376466a1132dfc90cf7e7aea1e47
mahmoud abdelhamid <mahmoud.abdelhamid@thinktech-it.com>
2025-07-13 18:49:29 +0300

    Refactor configuration handling and update welcome endpoint to use settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 import os 
+from helpers.config import get_settings,Settings
 
 base_router=APIRouter(
     prefix="/api/v1",
@@ -8,10 +9,12 @@ base_router=APIRouter(
 
 
 @base_router.get("/")
-async def welcome():
+async def welcome(app_setting:Settings=Depends(get_settings)):
     
+    app_name=app_setting.APP_NAME
+    app_version=app_setting.APP_VERSION
     return{
         "message":"Welcom to my rag app production",
-        "appName": os.getenv("APP_NAME"),
-        "appVersion":os.getenv("APP_VERSION")
+        "appName": app_name,
+        "appVersion":app_version
     }
